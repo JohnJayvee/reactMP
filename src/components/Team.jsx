aee2f948539051ae3edb009c3a5533c135ead873
JohnJayvee <siuagan.jayvee@gmail.com>
2024-06-28 19:33:28 +0800

    add props type validation

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
+// import 'slick-carousel/slick/slick-theme.css'; // Uncomment if needed
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'; // Importing brand icons
-import image from '../img/blog-1.jpg'; // Example image import
+import PropTypes from 'prop-types';
+import axios from 'axios';
 
-const NextArrow = ({ className, style, onClick }) => (
+import image from '../img/blog-1.jpg';
+
+
+const NextArrow = ({ className, onClick }) => (
     <div
         className={className}
         style={{
@@ -28,7 +32,14 @@ const NextArrow = ({ className, style, onClick }) => (
     </div>
 );
 
-const PrevArrow = ({ className, style, onClick }) => (
+NextArrow.propTypes = {
+    className: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+};
+
+
+
+const PrevArrow = ({ className, onClick }) => (
     <div
         className={className}
         style={{
@@ -47,9 +58,37 @@ const PrevArrow = ({ className, style, onClick }) => (
             icon={faArrowLeft}
         />
     </div>
+
 );
 
+PrevArrow.propTypes = {
+    className: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+};
+
 const TeamSection = () => {
+    const [teamMembers, setTeamMembers] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://kodegoapi.test/api/staffs', {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => {
+                console.log('API Response:', response);
+                // Ensure the response data has a staffs array
+                if (response.data.success && Array.isArray(response.data.staffs)) {
+                    setTeamMembers(response.data.staffs);
+                } else {
+                    console.error('Unexpected response format:', response.data);
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching the team members:', error);
+            });
+    }, []);
+
     const settings = {
         dots: false,
         infinite: true,
@@ -85,52 +124,7 @@ const TeamSection = () => {
         ]
     };
 
-    const teamMembers = [
-        {
-            image: 'img/team-1.jpg',
-            name: 'Dr. Angel C. Alcala',
-            position: 'Veterinarian',
-            social: [
-                { icon: faFacebookF, link: 'https://www.facebook.com' },
-                { icon: faTwitter, link: 'https://www.twitter.com' },
-                { icon: faInstagram, link: 'https://www.instagram.com' }
-            ]
-        },
-        {
-            image: 'img/team-2.jpg',
-            name: 'Dr. Ma. Gracia Dizon-Flores',
-            position: 'Veterinarian',
-            social: [
-                { icon: faFacebookF, link: 'https://www.facebook.com' },
-                { icon: faTwitter, link: 'https://www.twitter.com' },
-                { icon: faInstagram, link: 'https://www.instagram.com' }
-            ]
-        },
-        {
-            image: 'img/team-2.jpg',
-            name: 'Dr. Ma. Gracia Dizon-Flores',
-            position: 'Veterinarian',
-            social: [
-                { icon: faFacebookF, link: 'https://www.facebook.com' },
-                { icon: faTwitter, link: 'https://www.twitter.com' },
-                { icon: faInstagram, link: 'https://www.instagram.com' }
-            ]
-        },
-        {
-            image: 'img/team-2.jpg',
-            name: 'Dr. Ma. Gracia Dizon-Flores',
-            position: 'Veterinarian',
-            social: [
-                { icon: faFacebookF, link: 'https://www.facebook.com' },
-                { icon: faTwitter, link: 'https://www.twitter.com' },
-                { icon: faInstagram, link: 'https://www.instagram.com' }
-            ]
-        },
-        // Add more team members as needed
-    ];
-
     return (
-        // <div className="container mx-auto p-4 pt-6 md:p-6 lg:p-12">
         <div className="container mx-auto p-4 mt-20">
             <div className="border-l-4 border-blue-700 pl-4 mb-4 md:mb-6 lg:mb-12">
                 <h6 className="text-blue-700 uppercase">Team Members</h6>
@@ -140,14 +134,18 @@ const TeamSection = () => {
                 {teamMembers.map((member, index) => (
                     <div key={index} className="team-item p-2">
                         <div className="relative overflow-hidden group">
-                            <img src={image} alt={member.name} className="w-full transition-transform duration-500 transform group-hover:scale-105" />
+                            <img src={image || 'default-image-path.jpg'} alt={member.name} className="w-full transition-transform duration-500 transform group-hover:scale-105" />
                             <div className="team-overlay absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-500">
                                 <div className="flex space-x-2">
-                                    {member.social.map((socialLink, idx) => (
-                                        <a key={idx} href={socialLink.link} target="_blank" rel="noopener noreferrer" className="text-white w-10 h-10 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-600 transition duration-300">
-                                            <FontAwesomeIcon icon={socialLink.icon} />
-                                        </a>
-                                    ))}
+                                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="text-white w-10 h-10 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-600 transition duration-300">
+                                        <FontAwesomeIcon icon={faFacebookF} />
+                                    </a>
+                                    <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="text-white w-10 h-10 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-600 transition duration-300">
+                                        <FontAwesomeIcon icon={faTwitter} />
+                                    </a>
+                                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="text-white w-10 h-10 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-600 transition duration-300">
+                                        <FontAwesomeIcon icon={faInstagram} />
+                                    </a>
                                 </div>
                             </div>
                         </div>
@@ -161,5 +159,6 @@ const TeamSection = () => {
         </div>
     );
 };
+
 
 export default TeamSection;
