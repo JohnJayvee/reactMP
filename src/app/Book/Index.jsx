8d94388ec1e7c47ca1f379e1db86d97e919279d8
JohnJayvee <siuagan.jayvee@gmail.com>
2024-07-01 11:55:26 +0800

    add booking landing page

---
+++ src/app/Book/Index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import BookingForm from '../../components/BookingForm';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+const Order = () => {
+    return (
+        <>
+            <Header />
+            <BookingForm />
+            <Footer />
+
+        </>)
+}
+
+export default Order
