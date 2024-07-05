020078cfe5475079a5ac1dcd54a5598072f4d5bd
JohnJayvee <siuagan.jayvee@gmail.com>
2024-07-05 17:54:05 +0800

    add inactivity timeout

--- src/app/Index.jsx
+++ src/app/Index.jsx
@@ -5,32 +5,36 @@ import Carousel from '../layout/Carousel';
 import Admission from '../components/Admission';
 import useAuth from '../components/Auth';
 import useInactivityTimeout from '../components/useInactivityTimeout'
-// import ExpandableText from '../components/ExpandableText';
 import Feedback from '../layout/Feedback';
 import Overview from '../layout/Overview';
 import WebDev from '../layout/WebDev';
 
 
+
 const HomePage = () => {
     const isLoggedIn = useAuth();
 
-
     useInactivityTimeout(() => {
-        localStorage.removeItem("token");
-        window.location.reload();
+        if (sessionStorage.getItem("token")) {
+            sessionStorage.removeItem("token");
+            window.location.reload();
+        } else if (localStorage.getItem("token")) {
+            return null;
+            // Handle the case where you don't want to remove from localStorage
+            // Example: localStorage.removeItem("token");
+            // Or do nothing if you intend to keep it in localStorage
+        }
     });
 
-    // if (!isLoggedIn) {
-    //     alert('Please Login');
-
-    // }
+    if (!isLoggedIn) {
+        return null;
+    }
 
 
     return (
         <div>
             <Header />
             <Carousel />
-            {/* <ExpandableText maxChars={20}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil corrupti animi voluptates alias quaerat facere facilis sint dolore ipsam, hic veritatis! Perspiciatis, quam vel error dicta dolorum doloribus quod a.</ExpandableText> */}
             <Overview />
             <WebDev />
             <Feedback />
