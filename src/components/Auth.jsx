60780e45623a9d91bfd5d7d28377c83d936e3836
JohnJayvee <siuagan.jayvee@gmail.com>
2024-07-05 17:29:34 +0800

    add localStorage & sessionStorage Token

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,22 +1,16 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-
-
-
 const useAuth = () => {
   const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-
-
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('token') || sessionStorage.getItem('token');
     if (!token) {
       navigate('/login', { replace: true });
     } else {
       setIsLoggedIn(true);
-      localStorage.getItem('token');
     }
   }, [navigate]);
 
