e2a71a2e59b7cc1078bd58f0b826eda700dd66a0
JohnJayvee <siuagan.jayvee@gmail.com>
2024-07-08 12:59:00 +0800

    Remove auth home page

--- src/app/Index.jsx
+++ src/app/Index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Header from '../layout/Header';
 import Footer from '../layout/Footer';
 import Carousel from '../layout/Carousel';
-import useAuth from '../components/Auth';
-import useInactivityTimeout from '../components/useInactivityTimeout'
+// import useAuth from '../components/Auth';
+// import useInactivityTimeout from '../components/useInactivityTimeout'
 import Feedback from '../layout/Feedback';
 import Overview from '../layout/Overview';
 import WebDev from '../layout/WebDev';
@@ -11,20 +11,20 @@ import WebDev from '../layout/WebDev';
 
 
 const HomePage = () => {
-  const isLoggedIn = useAuth();
+  // const isLoggedIn = useAuth();
 
-  useInactivityTimeout(() => {
-    if (sessionStorage.getItem("token")) {
-      sessionStorage.removeItem("token");
-      window.location.reload();
-    } else if (localStorage.getItem("token")) {
-      return null;
-    }
-  });
+  // useInactivityTimeout(() => {
+  //   if (sessionStorage.getItem("token")) {
+  //     sessionStorage.removeItem("token");
+  //     window.location.reload();
+  //   } else if (localStorage.getItem("token")) {
+  //     return null;
+  //   }
+  // });
 
-  if (!isLoggedIn) {
-    return null;
-  }
+  // if (!isLoggedIn) {
+  //   return null;
+  // }
 
 
   return (
