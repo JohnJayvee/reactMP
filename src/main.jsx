430e212fd590e249d43927cf951dac6367f65aa7
JohnJayvee <siuagan.jayvee@gmail.com>
2024-07-08 12:59:22 +0800

    implement react query

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,17 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 // import './index.css';
 import './styles.css'; // Import Tailwind CSS
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
